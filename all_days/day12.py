39b251948e93cae31a108cfaa810d081c15d5502
Valérie Mauduit <valerie@ufoot.org>
2021-01-03 22:28:32 +0100

    Day 12 - star 2

--- all_days/day12.py
+++ all_days/day12.py
@@ -20,18 +20,31 @@
 # What is the total energy in the system after simulating the moons given in your scan for 1000 steps?
 
 # Second star:
+# All this drifting around in space makes you wonder about the nature of the universe. Does history really repeat
+# itself? You're curious whether the moons will ever return to a previous state.
+# Determine the number of steps that must occur before all of the moons' positions and velocities exactly match a
+# previous point in time.
+
+from math import gcd
+
+def lcm(values):
+    while len(values) > 1:
+        pgcd = gcd(values[0], values[1])
+        values = [int(values[0] * values[1] / pgcd)] + values[2:]
+    return values[0]
+
 
 class Moon():
     def __init__(self, scan):
         coords = scan[1:-1].split(', ')
-        self.position = (int(coords[0].split('=')[1]), int(coords[1].split('=')[1]), int(coords[2].split('=')[1]))
+        self.position = (float(coords[0].split('=')[1]), float(coords[1].split('=')[1]), float(coords[2].split('=')[1]))
         self.velocity = (0, 0, 0)
 
     def printable_position(self):
-        return '<' + ', '.join([c + str(x) for x, c in zip(self.position, ['x=', 'y=', 'z='])]) + '>'
+        return '<' + ', '.join([c + f'{x:0.0f}' for x, c in zip(self.position, ['x=', 'y=', 'z='])]) + '>'
 
     def printable_velocity(self):
-        return '<' + ', '.join([c + str(x) for x, c in zip(self.velocity, ['x=', 'y=', 'z='])]) + '>'
+        return '<' + ', '.join([c + f'{x:0.0f}' for x, c in zip(self.velocity, ['x=', 'y=', 'z='])]) + '>'
 
     def printable_summary(self):
         return 'pos=' + self.printable_position() + ', vel=' + self.printable_velocity()
@@ -67,7 +80,9 @@ class Moon():
 class NBobySystem():
     def __init__(self, moons_list):
         self.moons_list = moons_list
-        self.n = len(moons_list)
+        self.initial_state = [body.printable_summary() for body in self.moons_list]
+        self.initial_positions = [body.position for body in self.moons_list]
+        self.initial_velocities = [body.velocity for body in self.moons_list]
 
     def apply_gravity(self):
         for body0 in self.moons_list:
@@ -83,6 +98,35 @@ class NBobySystem():
     def run_step(self):
         self.apply_gravity()
         self.apply_velocities()
+
+    def run_steps(self, nb, verbose=False):
+        for n in range(nb):
+            self.run_step()
+            if verbose:
+                print(self.printable_summary())
+
+    def loop_to_initial_state(self):
+        self.run_step()
+        step = 0
+        while self.printable_summary() != self.initial_state:
+            step += 1
+            self.run_step()
+        return step + 1
+
+    def loop_to_initial_state_1D(self, dimension):
+        self.run_step()
+        step = 0
+        positions = [body.position[dimension] for body in self.moons_list]
+        velocities = [body.velocity[dimension] for body in self.moons_list]
+        while (
+                (positions != [p[dimension] for p in self.initial_positions]) or
+                (velocities != [v[dimension] for v in self.initial_velocities])
+        ):
+            step += 1
+            self.run_step()
+            positions = [body.position[dimension] for body in self.moons_list]
+            velocities = [body.velocity[dimension] for body in self.moons_list]
+        return step + 1
 
     def total_energy(self):
         return sum([body.total_energy() for body in self.moons_list])
@@ -101,7 +145,15 @@ def run(data_dir, star):
         print(f'Star {star} - Total energy is {system.total_energy()}')
         return system.total_energy()
     elif star == 2:
-        print(f'Star {star} - ')
-        return
+        moonsX = NBobySystem([Moon(moon) for moon in moons])
+        valX = moonsX.loop_to_initial_state_1D(0)
+        moonsY = NBobySystem([Moon(moon) for moon in moons])
+        valY = moonsY.loop_to_initial_state_1D(1)
+        moonsZ = NBobySystem([Moon(moon) for moon in moons])
+        valZ = moonsZ.loop_to_initial_state_1D(2)
+        total_loops = lcm([valX, valY, valZ])
+
+        print(f'Star {star} - X loop size is {valX}, Y loop size is {valY}, Z loop size is {valZ} - so: {total_loops}')
+        return total_loops
     else:
         raise Exception('Star number must be either 1 or 2.')
--- test-day12.py
+++ test-day12.py
@@ -1,4 +1,5 @@
-from all_days.day12 import Moon, NBobySystem
+from all_days.day12 import Moon, NBobySystem, lcm
+
 
 def test_apply_gravity():
     ganymede = Moon('<x=3, y=0, z=0>')
@@ -19,6 +20,7 @@ def test_apply_velocity():
     if europa.printable_position() != '<x=-1, y=2, z=6>':
         raise Exception('Europe should have moved to position <x=-1, y=2, z=6>')
     print('-- Test Apply Velocity OK')
+
 
 def test_complete_4bodies_problem():
     print('-- Complete 4-body test 1')
@@ -75,15 +77,13 @@ def test_complete_4bodies_problem():
         ]
     ]
     for step in range(11):
-        print(f'Step {step}')
         summary = moons.printable_summary()
         for calc, exp in zip(summary, expected[step]):
             if calc != exp:
-                raise Exception(f'Moon summary expected {exp} - found {calc}')
+                raise Exception(f'Step {step}: Moon summary expected {exp} - found {calc}')
         if step < 10:
             moons.run_step()
 
-    print('Energy computations')
     potential_energies = [body.potential_energy() for body in moons.moons_list]
     expected = [6, 9, 10, 6]
     if potential_energies != expected:
@@ -155,22 +155,18 @@ def test_complete_4bodies_problem2():
             'pos=<x=-29, y=-11, z=-1>, vel=<x=-3, y=7, z=4>', 'pos=<x=16, y=-13, z=23>, vel=<x=7, y=1, z=1>'
         ],
     ]
-    print(f'Step 0 - Initialization')
     summary = moons.printable_summary()
     for calc, exp in zip(summary, expected[0]):
         if calc != exp:
             raise Exception(f'Moon summary expected {exp} - found {calc}')
 
     for step in range(10):
-        print(f'Step {(step + 1) * 10}')
-        for _ in range(10):
-            moons.run_step()
+        moons.run_steps(10)
         summary = moons.printable_summary()
         for calc, exp in zip(summary, expected[step + 1]):
             if calc != exp:
-                raise Exception(f'Moon summary expected {exp} - found {calc}')
+                raise Exception(f'Step {(step + 1) * 10}: Moon summary expected {exp} - found {calc}')
 
-    print('Energy computations')
     potential_energies = [body.potential_energy() for body in moons.moons_list]
     expected = [29, 32, 41, 52]
     if potential_energies != expected:
@@ -188,11 +184,104 @@ def test_complete_4bodies_problem2():
         raise Exception(f'Expecting total energy 179, found: {total_energy}')
 
 
+def test_system_loops_small():
+    print('-- 4-body system: time to return to initial state')
+    io = Moon('<x= -1, y=  0, z=  2>')
+    europa = Moon('<x=  2, y=-10, z= -7>')
+    ganymede = Moon('<x=  4, y= -8, z=  8>')
+    callisto = Moon('<x=  3, y=  5, z= -1>')
+    moons = NBobySystem([io, europa, ganymede, callisto])
+
+    expected = {
+        0: [
+           'pos=<x=-1, y=0, z=2>, vel=<x=0, y=0, z=0>', 'pos=<x=2, y=-10, z=-7>, vel=<x=0, y=0, z=0>',
+            'pos=<x=4, y=-8, z=8>, vel=<x=0, y=0, z=0>', 'pos=<x=3, y=5, z=-1>, vel=<x=0, y=0, z=0>'
+        ],
+        2770: [
+            'pos=<x=2, y=-1, z=1>, vel=<x=-3, y=2, z=2>', 'pos=<x=3, y=-7, z=-4>, vel=<x=2, y=-5, z=-6>',
+            'pos=<x=1, y=-7, z=5>, vel=<x=0, y=-3, z=6>', 'pos=<x=2, y=2, z=0>, vel=<x=1, y=6, z=-2>'
+        ],
+        2771: [
+            'pos=<x=-1, y=0, z=2>, vel=<x=-3, y=1, z=1>', 'pos=<x=2, y=-10, z=-7>, vel=<x=-1, y=-3, z=-3>',
+            'pos=<x=4, y=-8, z=8>, vel=<x=3, y=-1, z=3>', 'pos=<x=3, y=5, z=-1>, vel=<x=1, y=3, z=-1>'
+        ],
+        2772: [
+            'pos=<x=-1, y=0, z=2>, vel=<x=0, y=0, z=0>', 'pos=<x=2, y=-10, z=-7>, vel=<x=0, y=0, z=0>',
+            'pos=<x=4, y=-8, z=8>, vel=<x=0, y=0, z=0>', 'pos=<x=3, y=5, z=-1>, vel=<x=0, y=0, z=0>'
+        ],
+    }
+    summary = moons.printable_summary()
+    for calc, exp in zip(summary, expected[0]):
+        if calc != exp:
+            raise Exception(f'Moon summary expected {exp} - found {calc}')
+    moons.run_steps(2770, verbose=False)
+
+    summary = moons.printable_summary()
+    for calc, exp in zip(summary, expected[2770]):
+        if calc != exp:
+            raise Exception(f'Moon summary expected {exp} - found {calc}')
+    moons.run_step()
+    summary = moons.printable_summary()
+    for calc, exp in zip(summary, expected[2771]):
+        if calc != exp:
+            raise Exception(f'Moon summary expected {exp} - found {calc}')
+    moons.run_step()
+    summary = moons.printable_summary()
+    for calc, exp in zip(summary, expected[2772]):
+        if calc != exp:
+            raise Exception(f'Moon summary expected {exp} - found {calc}')
+
+    io = Moon('<x= -1, y=  0, z=  2>')
+    europa = Moon('<x=  2, y=-10, z= -7>')
+    ganymede = Moon('<x=  4, y= -8, z=  8>')
+    callisto = Moon('<x=  3, y=  5, z= -1>')
+    moons = NBobySystem([io, europa, ganymede, callisto])
+    if moons.loop_to_initial_state() != 2772:
+        raise Exception('Computation of number of steps to return initial state should provide 2772')
+
+    moonsX = NBobySystem([
+        Moon('<x=-1, y=0, z=2>'), Moon('<x=2, y=-10, z=-7>'), Moon('<x=4, y=-8, z=8>'), Moon('<x=3, y=5, z=-1>')
+    ])
+    valX = moonsX.loop_to_initial_state_1D(0)
+    moonsY = NBobySystem([
+        Moon('<x=-1, y=0, z=2>'), Moon('<x=2, y=-10, z=-7>'), Moon('<x=4, y=-8, z=8>'), Moon('<x=3, y=5, z=-1>')
+    ])
+    valY = moonsY.loop_to_initial_state_1D(1)
+    moonsZ = NBobySystem([
+        Moon('<x=-1, y=0, z=2>'), Moon('<x=2, y=-10, z=-7>'), Moon('<x=4, y=-8, z=8>'), Moon('<x=3, y=5, z=-1>')
+    ])
+    valZ = moonsZ.loop_to_initial_state_1D(2)
+    if lcm([valX, valY, valZ]) != 2772:
+        raise Exception('Smart computation of number of steps to return initial state should provide 2772')
+
+
+def test_system_loops_large():
+    print('-- 4-body system: time to return to initial state')
+
+    moonsX = NBobySystem([
+        Moon('<x=-8, y=-10, z=0>'), Moon('<x=5, y=5, z=10>'), Moon('<x=2, y=-7, z=3>'), Moon('<x=9, y=-8, z=-3>')
+    ])
+    valX = moonsX.loop_to_initial_state_1D(0)
+    moonsY = NBobySystem([
+        Moon('<x=-8, y=-10, z=0>'), Moon('<x=5, y=5, z=10>'), Moon('<x=2, y=-7, z=3>'), Moon('<x=9, y=-8, z=-3>')
+    ])
+    valY = moonsY.loop_to_initial_state_1D(1)
+    moonsZ = NBobySystem([
+        Moon('<x=-8, y=-10, z=0>'), Moon('<x=5, y=5, z=10>'), Moon('<x=2, y=-7, z=3>'), Moon('<x=9, y=-8, z=-3>')
+    ])
+    valZ = moonsZ.loop_to_initial_state_1D(2)
+    nb_steps = 4686774924
+    if lcm([valX, valY, valZ]) != nb_steps:
+        raise Exception(f'Smart computation of number of steps to return initial state should provide {nb_steps}')
+
+
 def main():
     test_apply_gravity()
     test_apply_velocity()
     test_complete_4bodies_problem()
     test_complete_4bodies_problem2()
+    test_system_loops_small()
+    test_system_loops_large()
 
 
 if __name__ == '__main__':
