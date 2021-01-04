16ba4b49be4d145bd30c6cacd782902c41d1e57b
Valérie Mauduit <valerie@ufoot.org>
2021-01-04 21:57:29 +0100

    Day 14 - star 2 - WIP no implem to be tested

--- all_days/day14.py
+++ all_days/day14.py
@@ -13,8 +13,29 @@
 # 1 FUEL?
 
 # Second star:
+# After collecting ORE for a while, you check your cargo hold: 1 trillion (1000000000000) units of ORE.
+# Given 1 trillion ORE, what is the maximum amount of FUEL you can produce?
+
+from copy import deepcopy
+
+
+def printable_formula(reactants, products):
+    formula = ''
+    for mol, nb in reactants.items():
+        if nb > 0:
+            formula = formula + str(nb) + ' ' + mol + ', '
+    formula = formula[:-2] + ' => '
+    for mol, nb in products.items():
+        if nb > 0:
+            formula = formula + str(nb) + ' ' + mol + ', '
+    return formula[:-2]
+
 
 class ChemicalReaction():
+    @classmethod
+    def from_dicts(cls, reactants, products, molecules):
+        return cls(printable_formula(reactants, products), molecules)
+
     def __init__(self, formula, molecules):
         self.initial_formula = formula
         self.molecules = molecules
@@ -28,8 +49,8 @@ class ChemicalReaction():
         self.products[mol] = int(nb)
         self.main_product = mol
 
-    def use_reaction(self, reaction):
-        if reaction.main_product in self.other_reactants():
+    def use_reaction(self, reaction, authorized):
+        if reaction.main_product in self.other_reactants(authorized):
             self.reactants = {mol: nb + nb0 for mol0, nb0 in reaction.reactants.items()
                               for mol, nb in self.reactants.items() if mol == mol0}
             self.products = {mol: nb + nb0 for mol0, nb0 in reaction.products.items()
@@ -43,19 +64,81 @@ class ChemicalReaction():
                 self.products[molecule] -= value
         return None
 
-    def other_reactants(self):
-        return [mol for mol, nb in self.reactants.items() if nb > 0 and mol != 'ORE']
+    def other_reactants(self, authorized):
+        return [mol for mol, nb in self.reactants.items() if nb > 0 and mol not in authorized]
 
     def print_formula(self):
-        formula = ''
-        for mol, nb in self.reactants.items():
-            if nb > 0:
-                formula = formula + str(nb) + ' ' + mol + ', '
-        formula = formula[:-2] + ' => '
-        for mol, nb in self.products.items():
-            if nb > 0:
-                formula = formula + str(nb) + ' ' + mol + ', '
-        print(formula[:-2])
+        print(printable_formula(self.reactants, self.products))
+
+
+class Reserve():
+    def __init__(self, nb_ores):
+        if nb_ores is None:
+            nb_ores = 1
+        self.reactants = {'ORE': nb_ores}
+
+    def present_molecules(self):
+        return [k for k in self.reactants if self.reactants[k] > 0]
+
+    def add(self, molecule, quantity):
+        if molecule in self.reactants:
+            self.reactants[molecule] += quantity
+        else:
+            self.reactants[molecule] = quantity
+
+    def coefs_for_reaction(self, reaction):
+        return {
+            mol: self.reactants[mol] // reaction.reactants[mol] for mol in self.reactants if reaction.reactants[mol] > 0
+        }
+
+    def product_fuel(self, fuel_reaction, other_reactions):
+        coefs = self.coefs_for_reaction(fuel_reaction)
+        coef = min(coefs)
+        while coef == 0:
+            molecules_ok = {                                # Molecules we can use to transform fuel_reaction
+                mol: fuel_reaction.reactants[mol] - self.reactants[mol]
+                for mol, c in coefs.items() if c > 0
+            }
+            for reaction in other_reactions:                # Transform to use other molecules we have in reserve
+                fuel_reaction.use_reaction(reaction, molecules_ok)
+            if coefs['ORE'] == 0:                           # Specific treatment for ORE
+                ore_reaction_found, n = False, 0
+                while not ore_reaction_found:
+                    reaction = other_reactions[n]
+                    if (reaction.reactants['ORE']) > 0 and (reaction.main_product in molecules_ok):
+                        fuel_reaction.reactants['ORE'] -= reaction.reactants['ORE']
+                        fuel_reaction.reactants[reaction.main_product] += reaction.products[reaction.main_product]
+                        ore_reaction_found = True
+            coefs = self.coefs_for_reaction(fuel_reaction)  # Update coefs for fuel_reaction
+            coef = min(coefs)
+        fuel_created = coef
+        for mol in self.reactants:
+            self.reactants[mol] = self.reactants[mol] - coef * fuel_reaction.reactants[mol]
+        for mol in fuel_reaction.products:
+            self.add(mol, fuel_reaction.products[mol] * coef)
+        self.reactants['FUEL'] = 0
+        return fuel_created
+
+    def get_ores(self, reactions):
+        has_changed = True
+        while has_changed:
+            has_changed = False
+            for reaction in reactions:
+                if reaction.main_product in self.present_molecules():
+                    molecule = reaction.main_product
+                    if reaction.products[molecule] <= self.reactants[molecule]:
+                        has_changed = True
+                        coef = self.reactants[molecule] // reaction.products[molecule]
+                        self.reactants[molecule] = self.reactants[molecule] % reaction.products[molecule]
+                        for mol in reaction.reactants:
+                            self.add(mol, coef * reaction.reactants[mol])
+
+    def print(self):
+        text = ' + '.join([' '.join([str(v), k]) for k, v in self.reactants.items() if v > 0])
+        if len(text) == 0:
+            print('Empty reserve')
+        else:
+         print(text)
 
 
 def find_all_molecules(formulas):
@@ -63,27 +146,45 @@ def find_all_molecules(formulas):
     all_values = list(set([all_values[n] for n in range(1, len(all_values), 2)]))
     return all_values
 
-def compute_ores(formulas):
-    all_molecules = find_all_molecules(formulas)
-    reactions = [ChemicalReaction(formula, all_molecules) for formula in formulas]
+
+def chained_reactions(reactions, authorized=None):
+    if authorized == None:
+        authorized = ['ORE']
     final_reaction = [reaction for reaction in reactions if reaction.products['FUEL'] > 0][0]
     reactions = [reaction for reaction in reactions if reaction != final_reaction]
-    while len(final_reaction.other_reactants()) > 0:
+    while len(final_reaction.other_reactants(authorized)) > 0:
         for reaction in reactions:
-            final_reaction.use_reaction(reaction)
-    final_reaction.print_formula()
-    return final_reaction.reactants['ORE']
+            final_reaction.use_reaction(reaction, authorized)
+    return final_reaction
+
+
+def compute_total_fuel(reactions, total_ores):
+    reserve = Reserve(total_ores)
+    initial_reactions = deepcopy(reactions)
+    fuel_reaction = chained_reactions(reactions)
+    total_fuel = reserve.product_fuel(fuel_reaction)
+
+    fuel_added = total_fuel
+    while (fuel_added > 0) | (len(reserve.present_molecules()) > 1) :
+        reactions = deepcopy(initial_reactions)
+        fuel_reaction = chained_reactions(reactions, reserve)
+        fuel_added = reserve.product_fuel(fuel_reaction)
+        total_fuel += fuel_added
+    return total_fuel
 
 
 def run(data_dir, star):
     with open(f'{data_dir}/input-day14.txt', 'r') as fic:
-        reactions =  fic.read().strip('\n').split('\n')
+        formulas =  fic.read().strip('\n').split('\n')
+    all_molecules = find_all_molecules(formulas)
+    reactions = [ChemicalReaction(formula, all_molecules) for formula in formulas]
     if star == 1:
-        ores = compute_ores(reactions)
+        ores = chained_reactions(reactions).reactants['ORE']
         print(f'Star {star} - you need {ores} ORE molecules')
-        return
-    elif star == 2:
-        print(f'Star {star} - ')
+        return ores
+    elif star == 2: # 2269324 too low
+        total_fuel = compute_total_fuel(reactions, 1000000000000)
+        print(f'Star {star} - I will produce {total_fuel} FUEL')
         return
     else:
         raise Exception('Star number must be either 1 or 2.')
--- test-day14.py
+++ test-day14.py
@@ -1,7 +1,8 @@
-from all_days.day14 import compute_ores
+from all_days.day14 import chained_reactions, compute_total_fuel, find_all_molecules, ChemicalReaction
 
-def test_examples():
-    reactions = [
+def test_star1():
+    print('=== Tests for star 1 ===')
+    formulas_lists = [
         [
             '10 ORE => 10 A', '1 ORE => 1 B', '7 A, 1 B => 1 C', '7 A, 1 C => 1 D', '7 A, 1 D => 1 E',
             '7 A, 1 E => 1 FUEL'
@@ -34,15 +35,54 @@ def test_examples():
         ]
     ]
     expected = [31, 165, 13312, 180697, 2210736]
-    for n in range(len(reactions)):
-        ore = compute_ores(reactions[n])
+    for n in range(len(formulas_lists)):
+        all_molecules = find_all_molecules(formulas_lists[n])
+        reactions = [ChemicalReaction(formula, all_molecules) for formula in formulas_lists[n]]
+        ore = chained_reactions(reactions).reactants['ORE']
         if ore != expected[n]:
             raise Exception(f'ORE needed should be {expected[n]}, you computed {ore}')
         print(f'Example {n} OK')
 
 
+def test_star2():
+    print('=== Tests for star 2 ===')
+    formulas_lists = [
+        [
+            '157 ORE => 5 NZVS', '165 ORE => 6 DCFZ', '44 XJWVT, 5 KHKGT, 1 QDVJ, 29 NZVS, 9 GPVTF, 48 HKGWZ => 1 FUEL',
+            '12 HKGWZ, 1 GPVTF, 8 PSHF => 9 QDVJ', '179 ORE => 7 PSHF', '177 ORE => 5 HKGWZ',
+            '7 DCFZ, 7 PSHF => 2 XJWVT', '165 ORE => 2 GPVTF', '3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT'
+        ],
+        [
+            '2 VPVL, 7 FWMGM, 2 CXFTF, 11 MNCFX => 1 STKFG', '17 NVRVD, 3 JNWZP => 8 VPVL',
+            '53 STKFG, 6 MNCFX, 46 VJHF, 81 HVMC, 68 CXFTF, 25 GNMV => 1 FUEL', '22 VJHF, 37 MNCFX => 5 FWMGM',
+            '139 ORE => 4 NVRVD', '144 ORE => 7 JNWZP', '5 MNCFX, 7 RFSQX, 2 FWMGM, 2 VPVL, 19 CXFTF => 3 HVMC',
+            '5 VJHF, 7 MNCFX, 9 VPVL, 37 CXFTF => 6 GNMV', '145 ORE => 6 MNCFX', '1 NVRVD => 8 CXFTF',
+            '1 VJHF, 6 MNCFX => 4 RFSQX',  '176 ORE => 6 VJHF'
+        ],
+        [
+            '171 ORE => 8 CNZTR', '7 ZLQW, 3 BMBT, 9 XCVML, 26 XMNCP, 1 WPTQ, 2 MZWV, 1 RJRHP => 4 PLWSL',
+            '114 ORE => 4 BHXH', '14 VRPVC => 6 BMBT',
+            '6 BHXH, 18 KTJDG, 12 WPTQ, 7 PLWSL, 31 FHTLT, 37 ZDVW => 1 FUEL',
+            '6 WPTQ, 2 BMBT, 8 ZLQW, 18 KTJDG, 1 XMNCP, 6 MZWV, 1 RJRHP => 6 FHTLT',
+            '15 XDBXC, 2 LTCX, 1 VRPVC => 6 ZLQW', '13 WPTQ, 10 LTCX, 3 RJRHP, 14 XMNCP, 2 MZWV, 1 ZLQW => 1 ZDVW',
+            '5 BMBT => 4 WPTQ', '189 ORE => 9 KTJDG', '1 MZWV, 17 XDBXC, 3 XCVML => 2 XMNCP',
+            '12 VRPVC, 27 CNZTR => 2 XDBXC', '15 KTJDG, 12 BHXH => 5 XCVML', '3 BHXH, 2 VRPVC => 7 MZWV',
+            '121 ORE => 7 VRPVC', '7 XCVML => 6 RJRHP', '5 BHXH, 4 VRPVC => 5 LTCX'
+        ]
+    ]
+    expected = [82892753, 5586022, 460664]
+    for n in range(len(formulas_lists)):
+        all_molecules = find_all_molecules(formulas_lists[n])
+        reactions = [ChemicalReaction(formula, all_molecules) for formula in formulas_lists[n]]
+        total_fuel = compute_total_fuel(reactions, 1000000000000)
+        if total_fuel != expected[n]:
+            raise Exception(f'FUEL produced should be {expected[n]}, you computed {total_fuel}')
+        print(f'Example {n} OK')
+
+
 def main():
-    test_examples()
+    # test_star1()
+    test_star2()
 
 
 if __name__ == '__main__':
